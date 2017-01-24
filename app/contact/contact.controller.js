e19ae9c64d5db25ae223bdf7023d4a101404918f
SimonComeau <sim_com1@hotmail.com>
2017-01-24 16:46:17 -0500

    hover addition

--- app/contact/contact.controller.js
+++ app/contact/contact.controller.js
@@ -3,7 +3,7 @@ let contactController = function ($scope) {
     $scope.contact = {};
     $scope.sendButton = function () {
         console.log("send button clicked by: " + $scope.contact.name +" email address is: "
-            + $scope.contact.email +" phone number is: "+ $scope.contact.phoneNumber);
+            + $scope.contact.email +" phone number is: "+ $scope.contact.phoneNumber + " message left: " + $scope.contact.message);
     };
     $scope.clearButton = function () {
         $scope.contact = {};
