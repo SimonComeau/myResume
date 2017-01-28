8400aa7ed6907380be5f837b5c4a08047713fc42
SimonComeau <sim_com1@hotmail.com>
2017-01-28 16:29:26 -0500

    set up db

---
+++ Database/SimonDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Data.Entity;
+using myResumeAPI.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Configuration = myResumeAPI.Migrations.Configuration;
+
+namespace myResumeAPI.Database
+{
+    public class SimonDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public SimonDbContext()
+        {
+            this.Database.Connection.ConnectionString = ConfigurationManager.ConnectionStrings["SimonDbContext"].ConnectionString;
+            //this.Database.Initialize(true);
+            Database.CreateIfNotExists();
+        }
+
+        public static SimonDbContext Create()
+        {
+            System.Data.Entity.Database.SetInitializer(new MigrateDatabaseToLatestVersion<SimonDbContext, Configuration>());
+            return new SimonDbContext();
+        }
+    }
+}
