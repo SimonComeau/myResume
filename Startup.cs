691bb36fd83b94e913bd108a21edf5f46b55c3b3
SimonComeau <sim_com1@hotmail.com>
2017-01-27 22:15:02 -0500

    fixed the website with a whats the worst that could happen

---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(myResumeAPI.Startup))]
+
+namespace myResumeAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
