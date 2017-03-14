af0481be0c867ab8bc7ab768c3079302060e7df7
SimonComeau <sim_com1@hotmail.com>
2017-03-14 09:54:15 -0400

    dotnet - refactoring removed test BN api

--- MailClients/OneAndOneSmtpClient.cs
+++ MailClients/OneAndOneSmtpClient.cs
@@ -3,16 +3,20 @@ using System.Net;
 using System.Net.Mail;
 using myResumeAPI.Interfaces;
 
-namespace myResumeAPI.MailClients {
-	public class OneAndOneSmtpClient : SmtpClient, IResumeMailClient {
-		public OneAndOneSmtpClient() {
+namespace myResumeAPI.MailClients
+{
+	public class OneAndOneSmtpClient : SmtpClient, IResumeMailClient
+	{
+		public OneAndOneSmtpClient()
+		{
 			Port = 587;
 			EnableSsl = true;
 			Host = "smtp.1and1.com";
 			Credentials = GetNetworkCredential();
 		}
 
-		NetworkCredential GetNetworkCredential() {
+		NetworkCredential GetNetworkCredential()
+		{
 			var userName = ConfigurationManager.AppSettings.Get("SmtpUserName");
 			var password = ConfigurationManager.AppSettings.Get("SmtpPassword");
 			return new NetworkCredential(userName, password);
